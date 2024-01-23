dacfddc3c6f74d1bd8538aae1aa6b52f344508c5
Thanapat Ussawanarong <thanapat.usw@gmail.com>
2024-01-23 15:32:19 +0700

    chore: setup project

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex h-dvh w-dvw items-center justify-center bg-gray-800">
+      <span className="animate-spin text-xl font-normal text-white">
+        Hi, mom!
+      </span>
+    </div>
+  );
+}
