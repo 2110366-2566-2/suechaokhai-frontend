500c4e6ad46d18c9800ba7d9a81dbfb1239a9173
WinSukon <jakrinsukon@gmail.com>
2024-01-30 18:07:21 +0700

    feat : added more style

--- src/components/OwnerInfo.tsx
+++ src/components/OwnerInfo.tsx
@@ -2,24 +2,25 @@ import Image from "next/image";
 
 const OwnerInfo = ({name,tel,mail,imgSrc}:{name:string,tel:string,mail:string,imgSrc:string}) => {
     return ( 
-        <div className="flex flex-row">
-            <div>Owner</div>
-            <div>
-            <Image
-                src={imgSrc}
-                width={100}
-                height={100}
-                alt="Picture of the author"
-            />
+        <div className="flex flex-col bg-white w-[792px]">
+            <div className="text-3xl">Owner</div>
+            <div className="flex flex-row">
+                <div className="h-[120px] w-[120px] relative m-3 justify-between items-center">
+                    <Image
+                        src={imgSrc}
+                        fill={true}
+                        alt="Picture of the author"
+                        // className=""
+                    />
+                </div>
+                <div className="flex-col">
+                    <div className="text-xl">{name}</div>
+                    <div>{tel}</div>
+                    <div>{mail}</div>
+                </div>
             </div>
-            <div className="flex-col">
-                <div>{name}</div>
-                <div>{tel}</div>
-                <div>{mail}</div>
-            </div>
-
-
         </div>
+        
     );
 }
  
