15bf9ff05fac82e7490f2589da2939cb8bd199a1
WinSukon <jakrinsukon@gmail.com>
2024-01-30 22:38:15 +0700

    feat : add mui icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,6 @@ export default function Home() {
       <span className="animate-spin text-xl font-normal text-white">
         Hi, mom!
       </span>
-      <div>wedrtyuiop;</div>
-      <OwnerInfo name="Thanaphat" tel='1234565456' mail="dfghjkl" imgSrc="/img/Boss.png"></OwnerInfo>
-
     </div>
   );
 }
--- src/components/OwnerInfo.tsx
+++ src/components/OwnerInfo.tsx
@@ -1,8 +1,10 @@
 import Image from "next/image";
+import LocalPhoneOutlinedIcon from '@mui/icons-material/LocalPhoneOutlined';
+import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
 
 const OwnerInfo = ({name,tel,mail,imgSrc}:{name:string,tel:string,mail:string,imgSrc:string}) => {
     return ( 
-        <div className="flex flex-col bg-white w-[500px]">
+        <div className="flex flex-col bg-white w-[700px]">
             <div className="text-4xl">Owner</div>
             <div className="flex flex-row  p-[8px] m-[8px] ">
                 <div className="h-[120px] w-[120px] relative justify-between items-center m-[8px] ">
@@ -15,8 +17,15 @@ const OwnerInfo = ({name,tel,mail,imgSrc}:{name:string,tel:string,mail:string,im
                 </div>
                 <div className="flex-col m-[8px]">
                     <div className="text-3xl">{name}</div>
-                    <div className="text-xl">{tel}</div>
-                    <div className="text-xl">{mail}</div>
+                    <div className="text-xl flex-row">
+                        <LocalPhoneOutlinedIcon className="m-1"></LocalPhoneOutlinedIcon> 
+                        {tel}
+                       
+                    </div>
+
+                    <div className="text-xl">
+                        <EmailOutlinedIcon className="m-1"></EmailOutlinedIcon>
+                        {mail}</div>
                 </div>
             </div>
         </div>
