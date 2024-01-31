2c3a9198dc64c2aebeebc04f31f5fe0807092614
grill <kanawatgrill2002@gmail.com>
2024-01-31 22:37:21 +0700

    refactor: clean

--- src/app/components/PropertyMap.tsx
+++ src/app/components/PropertyMap.tsx
@@ -1,13 +1,7 @@
 import React from "react";
 
-// let mapSrc = `https://www.google.com/maps/embed/v1/place?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}&q=Wishdom+Sathorn+90`;
-
 let googleApiKey = process.env.GOOGLE_MAPS_API_KEY;
 
-/*
-TODO: 
-- Use Google Maps API from .env
-*/
 const PropertyMap = ({ name }: { name: string }) => {
   return (
     <div>
