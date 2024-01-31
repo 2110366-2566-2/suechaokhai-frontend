14306725d6aead2dfbc182a8acefec2059713179
ChawinNg <ik.chawin@gmail.com>
2024-01-31 13:01:41 +0700

    fix: GoogleButton style fix

--- src/components/GoogleButton.tsx
+++ src/components/GoogleButton.tsx
@@ -13,20 +13,21 @@ export default function GoogleButton() {
         <div className="min-w-max px-2">Or continue with</div>
         <span className="w-full border-t border-[#B3B3B3]"></span>
       </div>
-
-      <button className="rounded-lg border border-[#B3B3B3] px-6 py-2">
-        <div className="flex w-full flex-row items-center justify-center">
-          <div className="flex w-full items-center justify-center ">
-            <Image
-              src={"/img/google-icon.png"}
-              width={48}
-              height={48}
-              alt="Google Icon"
-            />
-            <span className="text-xl font-semibold">Google</span>
+      <div className="flex items-center justify-center">
+        <button className="rounded-lg border border-[#B3B3B3] px-6 py-2">
+          <div className="flex w-full flex-row items-center justify-center">
+            <div className="flex w-full items-center justify-center gap-[24px]">
+              <Image
+                src={"/img/google-icon.png"}
+                width={48}
+                height={48}
+                alt="Google Icon"
+              />
+              <span className="text-xl font-semibold">Google</span>
+            </div>
           </div>
-        </div>
-      </button>
+        </button>
+      </div>
     </div>
   );
 }
