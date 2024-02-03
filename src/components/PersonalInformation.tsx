2ea622e64583f358a8da7dad6c5ee51bd6b8b648
jMnnnnn <pj_teme@hotmail.com>
2024-02-03 15:14:00 +0700

    feat: personal information w/o constraint

--- src/components/PersonalInformation.tsx
+++ src/components/PersonalInformation.tsx
@@ -12,14 +12,35 @@ export default function PersonalInformation() {
                 alt="profilePic"
                 width={267}
                 height={220}
-            >
-            </Image>
-            <form className="pt-[22px] px-[67px] text-left text-[20px]" action="">
-                <TextBox
-                    label="First Name">
-
-                </TextBox>
+            ></Image>
+            <form className="px-[67px] text-left text-[20px]" action="">
+                <div className="flex flex-col pt-[22px] gap-[22px]">
+                    <TextBox
+                    label="First Name"
+                    placeholder="Enter your first name"
+                    ></TextBox>
+                </div>
+                <div className="flex flex-col gap-[22px] pt-[22px]">
+                    <TextBox
+                    label="Last Name"
+                    placeholder="Enter your last name"
+                    ></TextBox>
+                </div>
+                <div className="flex flex-col gap-[22px] pt-[22px]">
+                    <TextBox
+                    label="Phone Number"
+                    placeholder="Enter your phone number"
+                    ></TextBox>
+                </div>
             </form>
+            <div className="grid grid-cols-2 gap-x-[130px] pt-[50px]">
+                <div><button className="h-[60px] w-[190px] rounded-[10px] bg-[#3AAEEF] font-bold text-white text-[24px]">
+                    Back
+                </button></div>
+                <div><button className="h-[60px] w-[190px] rounded-[10px] bg-[#3AAEEF] font-bold text-white text-[24px]">
+                    Next
+                </button></div>
+            </div>
         </div>
     )
 }
