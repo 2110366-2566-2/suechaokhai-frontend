a8b593b538ff7467a198a72db7da3de28c49c154
WinSukon <jakrinsukon@gmail.com>
2024-02-04 17:10:39 +0700

    feat : button only work when date is chosen and change button CSS

--- src/components/RoomTourRes.tsx
+++ src/components/RoomTourRes.tsx
@@ -9,6 +9,7 @@ import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 
 import { PickersDay, PickersDayProps ,DayCalendarSkeleton}  from '@mui/x-date-pickers';
 import { PickersLayout } from '@mui/x-date-pickers';
+import { useState } from 'react';
 const customPickerDate =({props,highlightedDays}:{props:PickersDayProps<Dayjs>,highlightedDays?: number[]})=>{
 
   return(
@@ -20,6 +21,8 @@ const customPickerDate =({props,highlightedDays}:{props:PickersDayProps<Dayjs>,h
 
 const RoomTourRes = () => {
     const today = dayjs();
+    const [date,setDate]=useState<Dayjs|null>(null);
+
     return (
         <div className="flex flex-col bg-white p-4 ">
           <div className='text-xl font-medium'>Room Tour Reservation</div>
@@ -33,17 +36,29 @@ const RoomTourRes = () => {
                 // slotProps={{day:{day:today}}}
                 
               /> */}
-              <DateCalendar minDate={today}></DateCalendar>
+              <DateCalendar minDate={today} onChange={(newDate)=>{setDate(newDate)}}></DateCalendar>
 
             </LocalizationProvider>
-            <div className="flex-row">
-              <button className="w-[50%] hover:bg-[#FFCE50] my-4 font-bold text-black py-2 px-4 rounded-md shadow shadow-violet-600/25 hover:shadow-violet-600/75"
+            {date ? <div className="flex-row">
+              <button className="w-[50%] hover:bg-[#DFDFDF] my-4 font-semibold text-black py-2 px-4 rounded-md shadow "
                           type="submit"
                   >Save</button>
-              <button className="w-[50%] hover:bg-[#FFCE50] my-4 font-bold text-black py-2 px-4 rounded-md shadow shadow-violet-600/25 hover:shadow-violet-600/75"
+              <button className="w-[50%] bg-[#3AAEEF] hover:bg-blue-800 my-4 font-semibold text-white py-2 px-4 rounded-md shadow "
                          type="submit"
                 >Reserve Now</button>
             </div>
+            :
+            <div className="flex-row">
+              <button className="w-[50%]  my-4 font-semibold  text-[#DFDFDF] py-2 px-4 rounded-md shadow "
+                          type="submit" disabled
+                  >Save</button>
+              <button className="w-[50%]  my-4 font-semibold  bg-[#DFDFDF] text-white py-2 px-4 rounded-md shadow"
+                         type="submit" disabled
+                >Reserve Now</button>
+            </div>
+            
+            }
+            
    
           </form>
           
