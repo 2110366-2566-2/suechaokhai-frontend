80cd7e449931498b2cb4f1c58743420910375a18
ChawinNg <ik.chawin@gmail.com>
2024-02-11 14:33:46 +0700

    fix: home page view button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import OwnerInfo from "@/components/propertyDesc/OwnerInfo";
 import RoomTourRes from "@/components/propertyDesc/RoomTourRes";
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div className="flex h-dvh w-dvw items-center justify-center bg-gray-800">
-      <span className="animate-spin text-xl font-normal text-white">
-        Hi, mom!
-      </span>
+      <Link href="/suechaokhai/propertyDescription">
+        <button className="h-[60px] w-[510px] rounded-[10px] bg-[#3AAEEF] font-bold text-white">
+          View Property
+        </button>
+      </Link>
     </div>
   );
 }
