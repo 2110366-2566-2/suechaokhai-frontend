d70beee6a1bdfdf6efcd2d0ef4aa371d811c80ea
ChawinNg <ik.chawin@gmail.com>
2024-02-13 00:09:44 +0700

    fix: edit profile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -55,6 +55,12 @@ export default function Home() {
           </button>
         </div>
       ) : null}
+
+      <Link href="/suechaokhai/editProfile">
+        <button className="h-[50px] rounded-[10px] bg-[#3AAEEF] px-2 font-bold text-white">
+          Edit Profile
+        </button>
+      </Link>
     </div>
   );
 }
