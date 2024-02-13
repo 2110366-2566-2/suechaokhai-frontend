5f193e58a88c72c0b9d844fa98e2ecb32bc32e57
oreo10baht <papigonwin001@gmail.com>
2024-02-13 12:41:28 +0700

    fix: image slider design

--- src/components/propertyDesc/ImageSlider.tsx
+++ src/components/propertyDesc/ImageSlider.tsx
@@ -6,51 +6,10 @@ interface ImageSliderProps {
 }
 
 const ImageSlider: React.FC<ImageSliderProps> = ({ images }) => {
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const [imageLength, setImageLength] = useState(0);
-  useEffect(() => {
-    const handleResize = () => {
-      setIsSmallScreen(window.innerWidth < 1024);
-    };
-
-    window.addEventListener("resize", handleResize);
-
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
-  const [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth < 1024);
-  useEffect(() => {
-    setImageLength(images.length % 2 == 1 ? images.length - 1 : images.length);
-    if (isSmallScreen) {
-      setImageLength(images.length);
-    }
-  }, [images.length, isSmallScreen]);
-  const goToNext = () => {
-    setCurrentIndex((prevIndex) => (prevIndex + 1) % imageLength);
-    console.log(imageLength);
-  };
-
-  const goToPrev = () => {
-    setCurrentIndex((prevIndex) => (prevIndex - 1 + imageLength) % imageLength);
-  };
-
-  const transformValue = isSmallScreen ? 100 : 50;
   return (
     <div className="flex items-center">
-      <button
-        className="m-2 -translate-y-1/2 transform cursor-pointer border-none bg-transparent text-[40px] text-black"
-        onClick={goToPrev}
-      >
-        {"<"}
-      </button>
-      <div className="relative my-[2%] w-[90%] overflow-hidden">
-        <div
-          className="flex items-center transition-transform duration-500 ease-in-out"
-          style={{
-            transform: `translateX(-${currentIndex * transformValue}%)`,
-          }}
-        >
+      <div className="relative my-[2%] w-[90%] overflow-scroll overflow-y-auto">
+        <div className="flex items-center">
           {images.map((image, index) => (
             <div
               key={index}
@@ -67,12 +26,6 @@ const ImageSlider: React.FC<ImageSliderProps> = ({ images }) => {
           ))}
         </div>
       </div>
-      <button
-        className="m-2 -translate-y-1/2 transform cursor-pointer border-none bg-transparent text-[40px] text-black"
-        onClick={goToNext}
-      >
-        {">"}
-      </button>
     </div>
   );
 };
