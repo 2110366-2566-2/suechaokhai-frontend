31dceaa67856e4635b390e5a718c7176ac1ad613
oreo10baht <papigonwin001@gmail.com>
2024-02-13 10:29:50 +0700

    fix: image slider buttons @sm

--- src/components/propertyDesc/ImageSlider.tsx
+++ src/components/propertyDesc/ImageSlider.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 interface ImageSliderProps {
   images: string[];
@@ -7,15 +7,36 @@ interface ImageSliderProps {
 
 const ImageSlider: React.FC<ImageSliderProps> = ({ images }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
-  const imgLen = images.length % 2 == 1 ? images.length - 1 : images.length;
+  const [imageLength, setImageLength] = useState(0);
+  useEffect(() => {
+    const handleResize = () => {
+      setIsSmallScreen(window.innerWidth < 768);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+  // const imgLen = images.length % 2 == 1 ? images.length - 1 : images.length;
+  const [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth < 768);
+  useEffect(() => {
+    setImageLength(images.length % 2 == 1 ? images.length - 1 : images.length);
+    if (isSmallScreen) {
+      setImageLength(images.length);
+    }
+  }, [images.length, isSmallScreen]);
   const goToNext = () => {
-    setCurrentIndex((prevIndex) => (prevIndex + 1) % imgLen);
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % imageLength);
+    console.log(imageLength);
   };
 
   const goToPrev = () => {
-    setCurrentIndex((prevIndex) => (prevIndex - 1 + imgLen) % imgLen);
+    setCurrentIndex((prevIndex) => (prevIndex - 1 + imageLength) % imageLength);
   };
 
+  const transformValue = isSmallScreen ? 100 : 50;
   return (
     <div className="flex items-center">
       <button
@@ -27,7 +48,9 @@ const ImageSlider: React.FC<ImageSliderProps> = ({ images }) => {
       <div className="relative my-[2%] w-[90%] overflow-hidden">
         <div
           className="flex items-center transition-transform duration-500 ease-in-out"
-          style={{ transform: `translateX(-${currentIndex * 50}%)` }}
+          style={{
+            transform: `translateX(-${currentIndex * transformValue}%)`,
+          }}
         >
           {images.map((image, index) => (
             <div
