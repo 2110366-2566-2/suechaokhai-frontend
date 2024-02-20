6d4bbda1e9d9600e578d6c8ce467ff4dec8bffee
oreo10baht <111551012+oreo10baht@users.noreply.github.com>
2024-02-20 19:24:21 +0700

    feat: empty credit card

--- src/components/edit-profile/FinancialPage.tsx
+++ src/components/edit-profile/FinancialPage.tsx
@@ -1,6 +1,7 @@
 import Dropdown from "../register-login/DropDown";
 import NumberTextField from "@/components/editProfile/NumberTextField";
 import { useState } from "react";
+import EmptyCard from "@/components/edit-profile/EmptyCard";
 const FinancialPage = () => {
   const [bankNumber, setBankNumber] = useState("");
   const options = ["1", "2", "3"];
@@ -9,6 +10,12 @@ const FinancialPage = () => {
     <div className="space-y-10">
       <div className=" text-[40px] font-bold">Financial Information</div>
       <div className=" text-[30px] font-semibold ">Credit Card</div>
+      <div className="flex flex-row flex-wrap gap-8">
+        <EmptyCard />
+        <EmptyCard />
+        <EmptyCard />
+        <EmptyCard />
+      </div>
       <div className=" text-[30px] font-semibold ">Bank Account</div>
       <div className="flex flex-row space-x-8">
         <Dropdown
