ace38861697f23548a4e133b754e9f3de3c9416b
WinSukon <jakrinsukon@gmail.com>
2024-02-21 16:12:58 +0700

    feat: prop card done

--- src/components/mylisting/PropertyCard.tsx
+++ src/components/mylisting/PropertyCard.tsx
@@ -10,7 +10,7 @@ const PropertyCard = ({project_name,renting,images,province,district}:PropertyDa
     const [fav,setFav]  = useState<boolean>(false);
 
     return ( 
-       <div className="w-2/5 h-[800px] rounded-lg m-8 shadow-xl  bg-[#ECECEC]">
+       <div className="w-2/5 h-[800px] rounded-lg m-8  bg-[#ECECEC]">
             <div className="w-full h-[300px] relative rounded-t-lg  ">
                 <Image src={images[0]} alt='Product Picture' fill={true} className="object-cover rounded-t-lg"></Image>
 
@@ -27,12 +27,38 @@ const PropertyCard = ({project_name,renting,images,province,district}:PropertyDa
 
                 </div>
                 <hr className="border-black "></hr>
+
                 <div className="flex flex-col my-4 mx-1 h-1/2 ">
                     <div className="w-full text-xl my-2 ">{district}, {province}</div>
                     <div className="w-full text-xl my-2 font-semibold">{renting.price_per_month}</div>
                 </div>
+
                 <hr className="border-black "></hr>
-                
+
+                <div className="flex flex-row h-1/6 items-center my-8 mx-1">
+                    <div className="flex flex-row items-center w-1/2 justify-around">
+                        <Image
+                            src="/img/mylisting/bed.svg"
+                            alt="bed"
+                            width={50}
+                            height={40}
+                        />
+                        <div className="text-xl">2 Bedrooms</div>
+                    </div>
+
+                    <div className="flex flex-row items-center w-1/2 justify-around">
+                        <Image
+                            src="/img/mylisting/arrow.svg"
+                            alt="arrow"
+                            width={50}
+                            height={40}
+                        />
+                        <div className="text-xl">60 m²</div>
+                    </div>
+                    
+                    
+
+                </div>
                 <div className="flex flex-col items-center mx-1 ">
                     <button
                     className="mx-0.5 bg-ci-blue my-2 w-full h-[60px] rounded-md text-xl px-4 font-semibold text-[#DFDFDF] shadow "
@@ -46,11 +72,7 @@ const PropertyCard = ({project_name,renting,images,province,district}:PropertyDa
                     Delete property
                     </button>
                 </div>
-            </div>
-            
-
-            
-            
+            </div>  
        </div>
         
      );
