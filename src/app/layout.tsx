7d5ef53452120e918d8398d0f8300c211ea8db29
Krittapas Rungsimontuchat <nineten609@gmail.com>
2024-03-01 17:16:03 +0700

    feat: toast standard and add toast in Rootlayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
+import { Toaster } from "@/components/ui/toaster";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -18,7 +19,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={poppins.className}>{children}</body>
+      <body className={poppins.className}>
+        <main>{children}</main>
+        <Toaster />
+      </body>
     </html>
   );
 }
