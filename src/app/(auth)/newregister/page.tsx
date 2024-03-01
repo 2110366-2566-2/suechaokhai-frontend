ffb3fe7f855e746bfdc87c600b3ce78d91e2ffad
Krittapas Rungsimontuchat <nineten609@gmail.com>
2024-03-01 16:35:53 +0700

    fix: background style color in register

--- src/app/(auth)/newregister/page.tsx
+++ src/app/(auth)/newregister/page.tsx
@@ -35,92 +35,109 @@ export default function RegisterForm() {
   function onSubmit(values: z.infer<typeof formSchema>) {
     console.log(values);
     if (values.password !== values.confirmPassword) {
-        console.log("Password and confirm password do not match");
-        return;
+      console.log("Password and confirm password do not match");
+      return;
     }
   }
   return (
-    <div className="flex min-h-full flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8">
-      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-[480px]">
-        <div className="bg-white px-6 py-12 shadow sm:rounded-lg sm:px-12">
-          <Image
-            src="/img/login-register/comp-icon.svg"
-            alt="test"
-            width={80}
-            height={80}
-            className="mx-auto h-10 w-auto"
-          />
-          <h2 className="mt-6 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-            Register
-          </h2>
-          <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-              <FormField
-                control={form.control}
-                name="email"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Email : </FormLabel>
-                    <FormControl>
-                      <Input placeholder="Enter your email here" {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="password"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Password : </FormLabel>
-                    <FormControl>
-                      <Input
-                        placeholder="Enter your password here"
-                        type="password"
-                        {...field}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="confirmPassword"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Confirm password : </FormLabel>
-                    <FormControl>
-                      <Input
-                        placeholder="Re-enter your password here"
-                        type="password"
-                        {...field}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <Button
-                type="submit"
-                className=" w-full bg-ci-blue font-bold text-white"
+    <>
+      <style jsx global>{`
+        body {
+          margin: 0;
+          padding: 0;
+          height: 100%;
+          background-color: #B8B8B8;
+        }
+
+        #__next {
+          height: 100%;
+        }
+      `}</style>
+      <div className="flex min-h-full flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8">
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-[480px]">
+          <div className="bg-white px-6 py-12 shadow sm:rounded-lg sm:px-12">
+            <Image
+              src="/img/login-register/comp-icon.svg"
+              alt="test"
+              width={80}
+              height={80}
+              className="mx-auto h-10 w-auto"
+            />
+            <h2 className="mt-6 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+              Register
+            </h2>
+            <Form {...form}>
+              <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className="space-y-8"
               >
-                Confirm
-              </Button>
-              <p className="text-center text-sm leading-6 text-gray-500">
-                Already have and account ? {" "}
-                <a
-                  href="/login"
-                className="font-semibol text-yellow-400 pl-3 sm:pl-4 lg:pl-5 hover:text-yellow-500 transition duration-150 ease-in-out" 
+                <FormField
+                  control={form.control}
+                  name="email"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Email : </FormLabel>
+                      <FormControl>
+                        <Input placeholder="Enter your email here" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="password"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Password : </FormLabel>
+                      <FormControl>
+                        <Input
+                          placeholder="Enter your password here"
+                          type="password"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="confirmPassword"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Confirm password : </FormLabel>
+                      <FormControl>
+                        <Input
+                          placeholder="Re-enter your password here"
+                          type="password"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <Button
+                  type="submit"
+                  className=" w-full bg-ci-blue font-bold text-white"
                 >
-                  Login
-                </a>
-              </p>
-            </form>
-          </Form>
+                  Confirm
+                </Button>
+                <p className="text-center text-sm leading-6 text-gray-500">
+                  Already have and account ?{" "}
+                  <a
+                    href="/login"
+                    className="font-semibol pl-3 text-yellow-400 transition duration-150 ease-in-out hover:text-yellow-500 sm:pl-4 lg:pl-5"
+                  >
+                    Login
+                  </a>
+                </p>
+              </form>
+            </Form>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
