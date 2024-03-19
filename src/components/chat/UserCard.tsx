ebd1e8fd70e6e02ab684136d99694e56d9bc6820
ChawinNg <ik.chawin@gmail.com>
2024-03-19 14:07:26 +0700

    fix: profile img

--- src/components/chat/UserCard.tsx
+++ src/components/chat/UserCard.tsx
@@ -3,12 +3,12 @@ import Image from "next/image";
 export default function UserCard({userId}:{userId:string}){
     return(
         <div className="flex flex-row w-full h-20 justify-start items-center hover:bg-ci-gray rounded-xl p-2">
-            <div className="aspect-square min-h-16 min-w-16 overflow-hidden rounded-full">
-                <Image src='/img/arthur.jpg' alt='รูปปก' 
-                width={12}
-                height={12}
+            <div className="h-16 w-16 overflow-hidden rounded-full">
+                <Image src='/img/babywinsmoking.jpg' alt='รูปปก' 
+                width={16}
+                height={16}
                 draggable={false}
-                className="min-h-16 min-w-16"/>
+                layout="responsive"/>
             </div>
             <div className="text-xl font-bold px-8">
                 {userId}
