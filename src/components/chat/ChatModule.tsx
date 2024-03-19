afd559e27027f34a1c288ec30e553463a03e7e7e
ChawinNg <ik.chawin@gmail.com>
2024-03-19 14:56:29 +0700

    feat: message box

--- src/components/chat/ChatModule.tsx
+++ src/components/chat/ChatModule.tsx
@@ -4,10 +4,13 @@ import { useEffect, useState } from "react";
 import UserData from "../models/UserData";
 import ChatIcon from "./ChatIcon";
 import ChatBox from "./ChatBox";
+import MessageBox from "./MessageModule";
 
 export default function ChatModule(){
     const [user,setUser] = useState<UserData>();
     const [isOpen,setOpen] = useState<boolean>(false);
+    const [isChat,setChat] = useState<boolean>(false);
+    const [chatWith,setChatWith] = useState<boolean>(false);
     useEffect(()=>{
         async function getUser(){
                 const user = await getCurrentUser();
@@ -22,6 +25,9 @@ export default function ChatModule(){
     <div className="flex right-0 bottom-0 fixed">
         {user&&
         <div className="flex flex-row justify-end items-end gap-x-6">
+            {isChat&&
+                <MessageBox/>
+            }
             {isOpen&&
                 <ChatBox setOpen={setOpen}/>
             }
--- src/components/chat/UserCard.tsx
+++ src/components/chat/UserCard.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function UserCard({userId}:{userId:string}){
     return(
         <div className="flex flex-row w-full h-20 justify-start items-center hover:bg-ci-gray rounded-xl p-2">
-            <div className="h-16 w-16 overflow-hidden rounded-full">
+            <div className="h-16 w-16 overflow-hidden rounded-full justify-center items-center flex">
                 <Image src='/img/babywinsmoking.jpg' alt='รูปปก' 
                 width={16}
                 height={16}
