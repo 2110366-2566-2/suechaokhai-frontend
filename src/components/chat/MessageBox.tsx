732766f344f6270dc6d46736bcc770b2cb2e4f89
Thanapat Ussawanarong <thanapat.usw@gmail.com>
2024-03-21 08:35:10 +0700

    fix: message box name

--- src/components/chat/MessageBox.tsx
+++ src/components/chat/MessageBox.tsx
@@ -17,15 +17,13 @@ export default function MessageBox({
   const chat: Chat = ChatService.getInstance().getCurrentChat();
 
   return (
-    <div className="flex h-[528px] w-96 flex-col gap-y-4 rounded-t-xl bg-white pb-2 shadow-xl shadow-slate-500">
-      <div className="flex flex-row items-center justify-between gap-2 px-6 py-4 shadow-lg shadow-slate-300">
-        <div className="flex flex-row items-center gap-x-6">
-          <div className="flex size-12 items-center justify-center overflow-hidden rounded-full">
-            <ProfileImage src={chat.profile_image_url} />
-          </div>
+    <div className="flex h-[528px] w-96 flex-col gap-4 rounded-t-xl bg-white pb-2 shadow-xl shadow-slate-500">
+      <div className="flex flex-row items-center justify-start gap-4 px-6 py-4 shadow-lg shadow-slate-300">
+        <div className="flex size-12 shrink-0 items-center justify-center overflow-hidden rounded-full">
+          <ProfileImage src={chat.profile_image_url} />
         </div>
-        <div className="truncate text-xl font-bold">{`${chat.first_name} ${chat.last_name}`}</div>
-        <div className="flex flex-row gap-x-4 text-lg">
+        <div className="w-full truncate text-xl font-bold">{`${chat.first_name} ${chat.last_name}`}</div>
+        <div className="flex shrink-0 flex-row gap-x-4 text-lg">
           <button className="rounded-md hover:bg-slate-300">
             <Image
               src="/img/chat/agreement.svg"
