8726693c67d5bc1d81c935df477207f26c559479
WinSukon <jakrinsukon@gmail.com>
2024-03-23 01:57:22 +0700

    feat: connect api in search page

--- src/app/(have-nav)/search/page.tsx
+++ src/app/(have-nav)/search/page.tsx
@@ -6,82 +6,34 @@ import SearchSection from "@/components/home-page/SearchSection";
 
 import { useEffect, useState } from "react";
 import SearchProperty from "@/components/my-listing/SearchProperty";
+import getProperties from "@/services/getProperties";
 
 const myFavPage = () => {
     const [propData,setData] = useState<PropertyData[]>([]);
-
-    function getRandomDate(): Date {
-        const startMillis = (new Date(1900, 0, 1)).getTime();
-        const endMillis = (new Date()).getTime();
-        const randomMillis = startMillis + Math.random() * (endMillis - startMillis);
-        return new Date(randomMillis);
-    }
-
-    function generatePropertyDataArray(n: number): PropertyData[] {
-      const propertyDataArray:PropertyData[]=[]
-      for (let i = 0; i < n; i++) {
-          const propertyData: PropertyData = {
-            address: "123 Main St",
-            alley: "Back Alley",
-            bathrooms: 2,
-            bedrooms: 3,
-            country: "SomeCountry",
-            created_at: getRandomDate().toDateString(),
-            district: "SomeDistrict",
-            floor: 2,
-            floor_size: 1500,
-            floor_size_unit: "sqft",
-            furnishing: "Furnished",
-            images: [{
-              created_at: getRandomDate().toDateString(),
-              url: "/img/Property.png"
-            }],
-            owner_id: "owner123",
-            postal_code: "12345",
-            property_id: "prop123",
-            property_description: "Lorem ipsum dolor sit amet...",
-            property_name: "Sample Property",
-            property_type: "House",
-            province: "SomeProvince",
-            renting: {
-              created_at: getRandomDate().toDateString(),
-              is_occupied: false,
-              price_per_month: Math.floor(Math.random() * 100000)
-            },
-            selling: {
-              created_at: getRandomDate().toDateString(),
-              is_sold: false,
-              price: 250000
-            },
-            street: "Main St",
-            sub_district: "SomeSubDistrict",
-            unit_number: 1
-          };
-          propertyDataArray.push(propertyData);
-        };
-      return propertyDataArray;
-  }
+    const [total,setTotal] = useState<number>(0);
 
     useEffect(()=>{
-        setData(generatePropertyDataArray(1000))
-    },[])
+        const fetchProp =  async ()=>{
+          const data = await getProperties("",20,1);
+          if(data){
+            setData(data.properties);
+            setTotal(data.total)
+          }
+          console.log(data)
+        }
+        fetchProp()
+    },[])    
 
-    const [haveProp,setHave] = useState<boolean>(true);
-    
-    const handleCreate=()=>{
-        console.log(haveProp)
-        setHave(!haveProp)
-    }
     return ( 
        <>
         {
-          propData.length !==0 ? (
+          total !==0 ? (
                 
               <div className="flex h-full flex-col mx-80  ">
                     <div className="px-48 ">
                       <SearchProperty></SearchProperty>
                     </div>
-                    <PropertyCards propData={propData} totalProp={1000} isEditable={false} additionaltext="from searching result" showAmount={true}></PropertyCards>
+                    <PropertyCards propData={propData} totalProp={total} isEditable={false} additionaltext="from searching result" showAmount={true}></PropertyCards>
 
               </div>
               ) : (
@@ -104,21 +56,6 @@ const myFavPage = () => {
                       Let&apos;s create a property on your listing.
                     </div>
                   </div>
-
-                  <button
-                    className="flex w-1/5 flex-row justify-around rounded-md bg-ci-blue p-4"
-                    onClick={handleCreate}
-                  >
-                    <Image
-                      src="/img/mylisting/plusCircle.svg"
-                      alt="add"
-                      width={30}
-                      height={30}
-                    />
-                    <div className="text-2xl font-bold text-white ">
-                      Create Property
-                    </div>
-                  </button>
                 </div>
       )}
     </>
