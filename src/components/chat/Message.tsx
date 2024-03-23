db2e3784cfc8b7a33befd46184287b0623aa66e9
Thanapat Ussawanarong <thanapat.usw@gmail.com>
2024-03-23 11:42:12 +0700

    feat: add message sent time

--- src/components/chat/Message.tsx
+++ src/components/chat/Message.tsx
@@ -1,11 +1,13 @@
 import { ChatMessage } from "@/models/chat";
 
 export default function Message({ message }: { message: ChatMessage }) {
+  const sentAt = new Date(message.sent_at);
   return (
     <div>
       <div
-        className={`flex items-center ${message.author ? "justify-end" : "justify-start"}`}
+        className={`flex items-end gap-2 ${message.author ? "justify-end" : "justify-start"}`}
       >
+        <div className="text-xs">{`${sentAt.getHours()}:${sentAt.getMinutes()}`}</div>
         <div className="rounded-xl bg-ci-light-gray px-4 py-2">
           {message.content}
         </div>
