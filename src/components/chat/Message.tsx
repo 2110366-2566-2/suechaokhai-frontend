67b28e19fc9df6d8e973a2591f6a33d95d1a87d9
Thanapat Ussawanarong <thanapat.usw@gmail.com>
2024-03-24 11:33:45 +0700

    fix: time zero padding

--- src/components/chat/Message.tsx
+++ src/components/chat/Message.tsx
@@ -2,18 +2,20 @@ import { ChatMessage } from "@/models/chat";
 
 const MessageStatus = ({ message }: { message: ChatMessage }) => {
   const sentAt = new Date(message.sent_at);
+  const hour = String(sentAt.getHours()).padStart(2, "0");
+  const minute = String(sentAt.getMinutes()).padStart(2, "0");
+
   return (
     <div className="flex h-full flex-col items-end justify-end">
       <div className="text-xs">
         {message.read_at !== null && message.author && "Read"}
       </div>
-      <div className="text-xs">{`${sentAt.getHours()}:${sentAt.getMinutes()}`}</div>
+      <div className="text-xs">{`${hour}:${minute}`}</div>
     </div>
   );
 };
 
 const MessageContent = ({ message }: { message: ChatMessage }) => {
-  const sentAt = new Date(message.sent_at);
   return (
     <div className="break-all rounded-xl bg-ci-light-gray px-4 py-2">
       {message.content}
