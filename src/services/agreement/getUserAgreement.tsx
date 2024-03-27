64f4e14dcfc47ac51e8eefbe186b2786c8abd549
ChawinNg <ik.chawin@gmail.com>
2024-03-27 22:37:20 +0700

    fix: error

--- src/services/agreement/getUserAgreement.tsx
+++ src/services/agreement/getUserAgreement.tsx
@@ -1,17 +1,16 @@
 export default async function getUserAgreement() {
-    try {
-      const response = await fetch(
-        `http://localhost:8000/api/v1/user/me/agreements`,
-        {
-          method: "GET",
-          credentials: "include"
-        }
-      );
-      const data = await response.json();
-      return data
-    } catch (error) {
-      console.error("Error fetching agreements:", error);
-      throw new Error("Failed to fetch agreements");
-    }
+  try {
+    const response = await fetch(
+      `http://localhost:8000/api/v1/user/me/agreements`,
+      {
+        method: "GET",
+        credentials: "include",
+      }
+    );
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching agreements:", error);
+    // throw new Error("Failed to fetch agreements");
   }
-  
+}
