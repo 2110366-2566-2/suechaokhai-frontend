f6bc39147ff1fa7c10818f332ac0175f21ea949a
WinSukon <jakrinsukon@gmail.com>
2024-03-27 15:50:06 +0700

    feat: delete popup responsive

--- src/components/my-listing/PropertyCard.tsx
+++ src/components/my-listing/PropertyCard.tsx
@@ -111,7 +111,7 @@ const PropertyCard = ({
         {editable ? (
           <div className=" flex flex-col items-center ">
             <button
-              className="small-text  in-card-button  bg-ci-blue "
+              className="small-text  in-card-button  bg-ci-blue"
               //! link to edit prop page
             >
               Edit details
@@ -141,18 +141,18 @@ const PropertyCard = ({
 
       {isDeleting ? (
         <div className="fixed left-[0] top-[0] z-40 flex h-[100vh] w-[100%] flex-col items-center justify-center bg-black bg-opacity-20">
-          <div className="relative flex h-2/5 w-1/3 flex-col items-center justify-around rounded-2xl bg-white p-[32px]">
-            <div className="text-4xl font-bold ">Delete a Property</div>
-            <div className="small-text mb-1 mt-10">
+          <div className="relative flex  flex-col items-center justify-around rounded-2xl bg-white p-auto p-10 m-10">
+            <div className="large-text font-bold ">Delete a Property</div>
+            <div className="small-text mb-1 mt-6 md:mt-8 lg:mt-10">
               Are you sure you want to delete this property ?
             </div>
-            <div className="medium-text mb-10 mt-1 font-semibold ">
+            <div className="medium-text mb-6 md:mb-8 lg:mb-10 mt-1 font-semibold ">
               {propData.property_name}
             </div>
 
-            <div className="flex-roe  flex w-full items-center justify-center">
+            <div className="flex w-full gap-x-5 flex-row items-center justify-center">
               <button
-                className="medium-text mx-2 my-2 h-[60px] w-1/3 rounded-md bg-[#B3B3B3] px-4 font-bold text-[#DFDFDF] shadow "
+                className="medium-text  in-card-button  bg-[#B3B3B3]  "
                 onClick={() => {
                   setDel(!isDeleting);
                 }}
@@ -160,7 +160,7 @@ const PropertyCard = ({
                 Cancel
               </button>
               <button
-                className="medium-text mx-2 my-2 h-[60px] w-1/3 rounded-md bg-ci-red px-4 font-bold text-[#DFDFDF] shadow "
+                className="medium-text in-card-button  bg-ci-red "
                 onClick={async () => {
                   setDel(!isDeleting);
                   const res = await deleteProperty(propData.property_id);
@@ -170,9 +170,8 @@ const PropertyCard = ({
                       description:
                         "Your property has been deleted successfully.",
                     });
-                    await new Promise((resolve)=>setTimeout(resolve,3000))
+                    await new Promise((resolve) => setTimeout(resolve, 2000));
                     window.location.href = "/listing";
-
                   }
                   console.log(res.message);
                 }}
@@ -183,7 +182,7 @@ const PropertyCard = ({
           </div>
         </div>
       ) : null}
-      <Toaster ></Toaster>
+      <Toaster></Toaster>
     </div>
   );
 };
